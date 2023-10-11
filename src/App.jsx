737f13f9dc5e604ee3b81c106013cd6b4da2c1a2
Hadayetullah <hadayetullah30@gmail.com>
2023-10-11 22:54:07 +0600

    WeatherDetail component completed to show detail view of Weather

--- src/App.jsx
+++ src/App.jsx
@@ -7,35 +7,59 @@ function App() {
   const API_KEY = "dc807ae661f2515cab64b72c530cb415";
 
   const latitudeLongitude = [
-    { city: 'dhaka', coords: 'lat=23.9536&lon=90.1495'},
-    { city: 'rajshahi', coords: 'lat=24.7106&lon=88.9414'},
-    { city: 'rangpur', coords: 'lat=25.8483&lon=88.9414'},
-    { city: 'mymensingh', coords: 'lat=24.7136&lon=90.4502'},
-    { city: 'sylhet', coords: 'lat=24.70506&lon=91.6761'},
-    { city: 'barisal', coords: 'lat=22.3811&lon=90.3372'},
-    { city: 'chattogram', coords: 'lat=23.1793&lon=91.9882'},
-    { city: 'khulna', coords: 'lat=22.8088&lon=89.2467'},
+    // Division
+    // { city: 'dhaka', coords: 'lat=23.9536&lon=90.1495'},
+    // { city: 'rajshahi', coords: 'lat=24.7106&lon=88.9414'},
+    // { city: 'rangpur', coords: 'lat=25.8483&lon=88.9414'},
+    // { city: 'mymensingh', coords: 'lat=24.7136&lon=90.4502'},
+    // { city: 'sylhet', coords: 'lat=24.70506&lon=91.6761'},
+    // { city: 'barisal', coords: 'lat=22.3811&lon=90.3372'},
+    // { city: 'chattogram', coords: 'lat=23.1793&lon=91.9882'},
+    // { city: 'khulna', coords: 'lat=22.8088&lon=89.2467'},
+
+    // District
+    { district: 'dhaka', coords: 'lat=23.8105&lon=90.3372'},
+    { district: 'faridpur', coords: 'lat=23.5424&lon=89.6309'},
+    { district: 'gazipur', coords: 'lat=23.9905&lon=90.3877'},
+    { district: 'gopalganj', coords: 'lat=23.0488&lon=89.8879'},
+    { district: 'kishoreganj', coords: 'lat=24.4260&lon=90.9821'},
+    { district: 'madaripur', coords: 'lat=23.2393&lon=90.1870'},
+    { district: 'manikganj', coords: 'lat=23.8617&lon=90.0003'},
+    { district: 'narayanganj', coords: 'lat=23.6429&lon=90.4883'},
+    { district: 'narsingdi', coords: 'lat=24.1344&lon=90.7860'},
+    { district: 'rajbari', coords: 'lat=23.7151&lon=89.5875'},
+    { district: 'shariatpur', coords: 'lat=23.2423&lon=90.4348'},
+    { district: 'tangail', coords: 'lat=24.3917&lon=89.9948'},
+
+    // City
+    // { city: 'khilkhet', coords: 'lat=23.8311&lon=90.4243'},
+    // { city: 'mirpur', coords: 'lat=23.8223&lon=90.3654'},
     // { city: 'khulna', coords: 'lat=22.6738&lon=89.3967'},
   ];
 
-  const stateNames = {
-    "Bangladesh": "Dhaka",
-    "Rangpur Division": "Rangpur",
-    "Pār Naogaon": "Rajshahi",
-    "Mymensingh": "Mymensingh",
-    "Sylhet Division": "Sylhet",
-    "Barisal Division": "Barisal",
-    "Khagrachhari": "Chattogram",
-    "Khulna Division": "Khulna"
+  const districtNames = {
+    "Dhaka District": "Dhaka",
+    "Tāngāil": "Tangail",
+    "Pālang": "shariatpur",
+    "Srīpur": "rajbari",
+    "Bājitpur": "narsingdi",
+    "Nārāyanganj": "narayanganj",
+    "Mānikganj": "manikganj",
+    "Mādārīpur": "madaripur",
+    "Kishorganj": "Kishorganj",
+    "Tungipāra": "gopalganj",
+    "Tongi": "gazipur",
+    "Farīdpur": "faridpur"
   }
   
   const [data, setData] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [weatherDetail, setWeatherDetail] = useState(null);
 
-    
+
 
   useEffect(() => {
+    
     const fetchData = async () => {
       if (currentIndex < latitudeLongitude.length) {
         const location = latitudeLongitude[currentIndex];
@@ -62,22 +86,38 @@ function App() {
   const handleWeatherDetail = (value) => {
     setWeatherDetail(value);
   }
+  function reloadApp() {
+    window.location.reload();
+  }
+  
 
-  console.log(data)
+  // console.log(data)
 
   return (
     <>
       <div className="container">
         <div className="weather">
-          <h5>Daily weather of different states in Bangladesh</h5>
+          <h5>Daily weather of different districts of Dhaka Division</h5>
+          <button onClick={reloadApp} className='refresh__button'>Refresh</button>
           <div className='weather__table'>
               {
                   data.map((state, i) => {
                     let stName = state.name;
                       return (
                           <button onClick={()=>handleWeatherDetail(i)} className='individual__state' key={i}>
-                              <p>{ stateNames[stName] }</p>
-                              <p>{ Math.ceil(state.main.temp - 273.15) }<sup>o</sup></p>
+                              <div className="individual__state__left">
+                                <p>{ districtNames[stName] }</p>
+                              </div>
+
+                              <div className="individual__state__right">
+                                <div className='individual__state__icon'>
+                                  <img src={`https://openweathermap.org/img/wn/${state.weather[0].icon}@2x.png`} alt="Weather Icon" />
+                                </div>
+
+                                <div className='individual__state__temp'>
+                                  <p>{ Math.ceil(state.main.temp - 273.15) }<sup>o</sup></p>
+                                </div>
+                              </div>
                           </button>
                       )
                   })
@@ -86,7 +126,7 @@ function App() {
         </div>
 
         {
-          weatherDetail != null ? <WeatherDetail detailData={data[weatherDetail]} handleWeatherDetail={handleWeatherDetail} /> : <div></div>
+          weatherDetail != null ? <WeatherDetail districtNames={districtNames} detailData={data[weatherDetail]} handleWeatherDetail={handleWeatherDetail} /> : <div></div>
         }
       </div>
     </>
--- src/WeatherDetail.jsx
+++ src/WeatherDetail.jsx
@@ -1,13 +1,97 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
-const WeatherDetail = ({detailData, handleWeatherDetail}) => {
-    console.log("Details: ",detailData)
+const WeatherDetail = ({districtNames, detailData, handleWeatherDetail}) => {
+    // console.log("Details: ",detailData)
+    // console.log("Cities: ",districtNames)
+    let stName = detailData.name;
   return (
     <div className='weather__detail'>
-        <div className="weather__detail__background">
-            <div className="weather__detail__header">Header</div>
-            <div className="weather__detail__content">
-            WeatherDetail
+        <div className="weather__detail__overflow">
+            <div className="weather__detail__background">
+                <div className="weather__detail__header">
+                    <div className="weather__detail__header__content">
+                        <h5>Weather Details of { districtNames[stName] } City</h5>
+                    </div>
+                    <div className="weather__detail__header__icon">
+                        <FontAwesomeIcon icon={faXmark} onClick={()=> handleWeatherDetail(null)} />
+                    </div>
+                </div>
+                <hr />
+                <div className="weather__detail__content">
+                    <div className="weather__detail__contents__top">
+                        <div className="weather__detail__contents__top__left">
+                            <div className="weather__detail__contents__top__icon">
+                                <img src={`https://openweathermap.org/img/wn/${detailData.weather[0].icon}@4x.png`} alt="Weather Icon" />
+                            </div>
+
+                            <div className="weather__detail__contents__top__sky">
+                                <p>{ detailData.weather[0].main }</p>
+                            </div>
+                        </div>
+                        
+                        <div className="weather__detail__contents__top__right">
+                            <div className="weather__detail__contents__top__temp">
+                                <div className="weather__detail__contents__top__temp__text">
+                                    <p>{ Math.ceil(detailData.main.temp - 273.15) }</p>
+                                </div>
+                                <div className="weather__unit__icons">
+                                <p style={{
+                                    fontSize: "30px",
+                                    fontWeight: "bolder"
+                                }}>o</p>
+                                    <p style={{
+                                        fontSize: "40px",
+                                        fontWeight: "normal"
+                                    }}>c</p>
+                                </div>
+                            </div>
+
+                            <div className="weather__detail__contents__top__feel">
+                                <p style={{paddingRight: "10px"}}>Real Feel<sup>*</sup></p>
+                                <p>{ Math.ceil(detailData.main.feels_like - 273.15) }<sup>o</sup></p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="weather__detail__contents__bottom">
+                        <div className="weather__detail__contents">
+                            <p>Current Temperature</p>
+                            <div><p>{ Math.ceil(detailData.main.temp - 273.15) }<sup>o</sup></p></div>
+                        </div>
+                        
+                        <div className="weather__detail__contents">
+                            <p>Humidity</p>
+                            <div><p>{ Math.ceil(detailData.main.humidity) }%</p></div>
+                        </div>
+
+                        <div className="weather__detail__contents">
+                            <p>Feel Like</p>
+                            <div><p>{ Math.ceil(detailData.main.feels_like - 273.15) }<sup>o</sup></p></div>
+                        </div>
+
+                        <div className="weather__detail__contents">
+                            <p>Weather Condition</p>
+                            <div><p>{ detailData.weather[0].description }</p></div>
+                        </div>
+
+                        <div className="weather__detail__contents">
+                            <p>Sea Level</p>
+                            <div><p>{ (((detailData.main.pressure) * 0.0145038) / 100).toFixed(3) }psi</p></div>
+                        </div>
+
+                        <div className="weather__detail__contents">
+                            <p>Maximum Temperature</p>
+                            <div><p>{ Math.ceil(detailData.main.temp_max - 273.15) }<sup>o</sup></p></div>
+                        </div>
+
+                        <div className="weather__detail__contents">
+                            <p>Minimum Temperature</p>
+                            <div><p>{ Math.ceil(detailData.main.temp_min - 273.15) }<sup>o</sup></p></div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
